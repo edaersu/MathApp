476c608257113dae1a085becd50751690c678c2f
edaersu <edaersu1@gmail.com>
2021-06-10 10:32:39 +0300

    added @BindView by importing butterknife

--- app/src/main/java/com/example/mathapp/MainActivity.java
+++ app/src/main/java/com/example/mathapp/MainActivity.java
@@ -1,55 +1,64 @@
 package com.example.mathapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements MainActivityContract.View{
+import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity implements MainActivityContract.View {
+
+
+    @BindView(R.id.tv1)
+    EditText tv1;
+    @BindView(R.id.tv2)
+    EditText tv2;
+    @BindView(R.id.btn_islem)
+    Button btnIslem;
+    @BindView(R.id.sonuc)
+    TextView sonuc;
 
     private MainActivityPresenter presenter;
-
-    TextView msonuc;
-    EditText mtv1,mtv2;
-    Button mbtn;
     Helper helper;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        helper=new Helper();
-        this.presenter=new MainActivityPresenter(helper);
+        helper = new Helper();
+        this.presenter = new MainActivityPresenter(helper);
         this.presenter.setView(this);
         this.presenter.created();
     }
 
     @Override
     public void bindView() {
-        mtv1=findViewById(R.id.tv1);
-        mtv2=findViewById(R.id.tv2);
-        msonuc=findViewById(R.id.sonuc);
-        mbtn=findViewById(R.id.btn_islem);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        // msonuc=findViewById(R.id.sonuc);
+        btnIslem = findViewById(R.id.btn_islem);
     }
 
     @Override
     public void initOnClick() {
-        this.mbtn.setOnClickListener(new View.OnClickListener() {
+        this.btnIslem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                presenter.onTryClick(mtv1.getText().toString(),mtv2.getText().toString());
+                presenter.onTryClick(tv1.getText().toString(), tv2.getText().toString());
             }
         });
     }
 
     @Override
     public void initView(String s) {
-        msonuc.setText(s);
+        sonuc.setText(s);
     }
 }
